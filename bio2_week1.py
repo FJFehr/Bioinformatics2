e22ec0d55ca09a22a65a8eb385efbd7d8faad5c1
Fabio <fhrfab001@myuct.ac.za>
2019-04-09 09:58:42 +0200

    Got working Compositionk

--- bio2_week1.py
+++ bio2_week1.py
@@ -1,4 +1,30 @@
 # Week1 code
 # Fabio Fehr
 
+def Compositionk(Text, k):
 
+    """
+    Input:
+    Text is a single genome sequence
+    k is the length of the kmer
+
+    Output:
+    A list of kmers in lexicographic order
+    """
+    composition_list = []
+
+    # First slice up into a list
+    for i in range(len(Text)-k +1):
+        composition_list.append(Text[i:i+k])
+
+    #Sort list making it lexicographic
+    composition_list.sort()
+
+    return(composition_list)
+
+def main():
+
+    print(Compositionk("CAATCCAAC", 5))
+
+if __name__ == "__main__":
+    main()
