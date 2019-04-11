5d9c4a6fe8df4a05bb6b80d975470843c36b4514
Fabio <fhrfab001@myuct.ac.za>
2019-04-11 18:32:30 +0200

    Finished Overlapping patterns problem

--- bio2_week1.py
+++ bio2_week1.py
@@ -49,10 +49,32 @@ def PathToGenome(path):
     return(genome)
 
 
+def Overlap(Patterns):
+    """
+     Input: A collection Patterns of k-mers.
+     Output: The overlap graph Overlap(Patterns).
+    """
+
+    # Define the placeholder
+    pattern_dict = {}
+
+    # Set up all our keys
+    for pattern in Patterns:
+        pattern_dict[pattern] = []
+
+    for pattern in Patterns:
+
+        for pattern_key in pattern_dict:
+            if (pattern_key[1:] == pattern[:-1]):
+                pattern_dict[pattern_key].append(pattern)
+
+    return(pattern_dict)
+
+
 def main():
 
-    # #'dataset_197_3.txt'
-    with open('dataset_198_3.txt', 'r') as myfile:
+    # #'dataset_198_10.txt'
+    with open('dataset_198_10.txt', 'r') as myfile:
         dat = myfile.read().replace('\n', ' ')
     myfile.close()
 
@@ -65,6 +87,29 @@ def main():
     #         print(f"{kmer}", file=text_file)
 
     # print(PathToGenome(dat))
+
+    overlap_dict = Overlap(dat)
+
+    # for key, val in overlap_dict.items():
+    #     if(val != []):
+    #         print(key, " => ", val)
+
+    # for item in overlap_dict:
+    #     if overlap_dict[item]:
+    #         print(item + " => " + overlap_dict[item][0])
+
+    #print(overlap_dict)
+
+    with open("Overlap_output.txt", "w") as text_file:
+        for key, val in overlap_dict.items():
+            if (val != []):
+                val_str = val[0]
+                if len(val) > 1:
+                    for i in range(len(val)-1):
+                        val_str = val_str + ", " + val[i+1]
+
+                string_temp = key + " -> " + val_str
+                print(f"{string_temp}", file=text_file)
 
 
 if __name__ == "__main__":
