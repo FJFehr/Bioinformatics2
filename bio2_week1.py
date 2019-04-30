5a7d3508851c25f35f4768dd5173717eddf4500f
Fabio <fhrfab001@myuct.ac.za>
2019-04-30 12:43:57 +0200

    Finished DeBruijn problem

--- bio2_week1.py
+++ bio2_week1.py
@@ -71,10 +71,39 @@ def Overlap(Patterns):
     return(pattern_dict)
 
 
+def DeBruijnk(Text, k):
+    """
+    Input: An integer k and a string Text.
+    Output: DeBruijnk(Text), in the form of an adjacency list.
+    Main idea is now we glue together the identical ones
+    """
+
+    # Nodes will be labeled with k-1mers
+    k = k-1
+
+    # We now have k-1mer nodes
+    nodes = Compositionk(Text, k)
+
+    # Now find the overlap between these nodes
+
+    # Define the placeholder
+    pattern_dict = {}
+
+    # Set up all our keys
+    for pattern in nodes:
+        pattern_dict[pattern] = []
+
+    # This is different as we only look at the next one
+    for i in range(len(Text)-k):
+        pattern_dict[Text[i:i+k]].append(Text[i+1:i+1+k])
+
+    return(pattern_dict)
+
+
 def main():
 
-    # #'dataset_198_10.txt'
-    with open('dataset_198_10.txt', 'r') as myfile:
+    # #'dataset_199_6.txt'
+    with open('test.txt', 'r') as myfile:
         dat = myfile.read().replace('\n', ' ')
     myfile.close()
 
@@ -88,8 +117,6 @@ def main():
 
     # print(PathToGenome(dat))
 
-    overlap_dict = Overlap(dat)
-
     # for key, val in overlap_dict.items():
     #     if(val != []):
     #         print(key, " => ", val)
@@ -98,10 +125,23 @@ def main():
     #     if overlap_dict[item]:
     #         print(item + " => " + overlap_dict[item][0])
 
-    #print(overlap_dict)
+    # print(overlap_dict)
 
-    with open("Overlap_output.txt", "w") as text_file:
-        for key, val in overlap_dict.items():
+    # with open("Overlap_output.txt", "w") as text_file:
+    #     for key, val in overlap_dict.items():
+    #         if (val != []):
+    #             val_str = val[0]
+    #             if len(val) > 1:
+    #                 for i in range(len(val)-1):
+    #                     val_str = val_str + ", " + val[i+1]
+
+    #             string_temp = key + " -> " + val_str
+    #             print(f"{string_temp}", file=text_file)
+
+    DeBruijnk_dict = DeBruijnk(dat[1], int(dat[0]))
+
+    with open("DeBruijnk_output.txt", "w") as text_file:
+        for key, val in DeBruijnk_dict.items():
             if (val != []):
                 val_str = val[0]
                 if len(val) > 1:
